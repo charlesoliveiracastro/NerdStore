215d3dcee431b7d49b7a07e3a6c7ea77df4eb762
charlesoliveiracastro <charlesoliveiracastro@gmail.com>
2022-04-19 22:11:23 -0300

    changes

--- src/services/NSE.Identidade.API/Models/UserViewModels.cs
+++ src/services/NSE.Identidade.API/Models/UserViewModels.cs
@@ -14,7 +14,7 @@ namespace NSE.Identidade.API.Models
         [Required(ErrorMessage = "O campo {0} é obrigatório!")]
         [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres!", MinimumLength = 6)]
         public string Password { get; set; }
-        [Compare("Password", ErrorMessage = "As senhas nao conferem!")]
+        [Compare("Password", ErrorMessage = "As senhas não conferem!")]
         public string PasswordConfirm { get; set; }
     }
 
